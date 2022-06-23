951625f31aacd36adc677a5d449d2b197c1f7cd2
zx-arch <renaldi.kristian@ti.ukdw.ac.id>
2022-06-23 23:13:50 +0700

    Membuat Aplikasi CRUD Film + Searching

---
+++ final_project_71190424/app/src/main/java/id/ac/ukdw/final_project_71190424/Film.kt
@@ -0,0 +1,7 @@
+package id.ac.ukdw.final_project_71190424
+
+
+data class Film(
+    var judul: String, var genre: String, var nm_produser: String, var poster: String,
+    var pemeran_utama_pertama: String, var pemeran_utama_kedua: String, var pemeran_utama_ketiga: String, var tahun_rilis: Int
+)
